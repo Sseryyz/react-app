1a6c56d91e0ed6a8cac29de7e3a298a2268031b7
Sseryyz <12serzhj@gmail.com>
2019-12-02 17:56:20 +0200

    learn how to use react

--- src/App.js
+++ src/App.js
@@ -2,22 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>
+  }
+  return <h1>Hello, stranger</h1>
+}
+
+const user1 = {
+  firstName: "Mar'ia",
+  lastName: "Morevna"
+};
+
+const user2 = {
+  firstName: "Vaslisa",
+  lastName: "Maeva"
+};
+
+const date = (<div>
+  <h1>Heil, mir!</h1>
+  <h2>It is {new Date().toLocaleTimeString()}.</h2>
+</div>);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="Content"> Content </div>
+
+        {getGreeting(user1)}
+
+        {date}
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
