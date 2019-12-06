af8604650ddbceabd27493b49d057240d7ec7cc4
Sseryyz <12serzhj@gmail.com>
2019-12-06 18:04:35 +0200

    learn react 6

--- src/App.js
+++ src/App.js
@@ -2,246 +2,173 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const scaleNames = {
-  c: 'Цельсия',
-  f: 'Фаренгейта'
-};
-
-class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);
-  }
-
+class ProductCategoryRow extends React.Component {
   render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
+    const category = this.props.category;
     return (
-        <fieldset>
-          <legend>Введите градусы по шкале {scaleNames[scale]}:</legend>
-          <input value={temperature}
-                 onChange={this.handleChange} />
-        </fieldset>
+        <tr>
+          <th colSpan="2">
+            {category}
+          </th>
+        </tr>
     );
   }
 }
 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value.toUpperCase()});
-  }
-
-  handleSubmit(event) {
-    console.log('Submited name: ' + this.state.value);
-    event.preventDefault();
-  }
-
+class ProductRow extends React.Component {
   render() {
+    const product = this.props.product;
+    const name = product.stocked ?
+        product.name :
+        <span style={{color: 'red'}}>
+        {product.name}
+      </span>;
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value}
-                 onChange={this.handleChange} />
-        </label>
-        <input type="Submit" value="Send" />
-      </form>
+        <tr>
+          <td>{name}</td>
+          <td>{product.price}</td>
+        </tr>
     );
   }
 }
 
-class EssayForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'Будьте любезны, напишите сочинение о вашем любимом DOM-элементе.'
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    console.log('Сочинение отправлено: ' + this.state.value);
-    event.preventDefault();
-  }
-
+class ProductTable extends React.Component {
   render() {
-    return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Сочинение:
-            <textarea value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Отправить" />
-        </form>
-    );
-  }
-}
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
 
-class FlavorForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'coconut'};
+    const rows = [];
+    let lastCategory = null;
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('Ваш любимый вкус: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Выберите ваш любимый вкус:
-            <select value={this.state.value} onChange={this.handleChange}>
-              <option value="grapefruit">Грейпфрут</option>
-              <option value="lime">Лайм</option>
-              <option value="coconut">Кокос</option>
-              <option value="mango">Манго</option>
-            </select>
-          </label>
-          <input type="submit" value="Отправить" />
-        </form>
-    );
-  }
-}
-
-class Reservation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isGoing: true,
-      numberOfGuest: 2
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
+    this.props.products.forEach((product) => {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+      if (product.category !== lastCategory) {
+        rows.push(
+            <ProductCategoryRow
+                category={product.category}
+                key={product.category} />
+        );
+      }
+      rows.push(
+          <ProductRow
+              product={product}
+              key={product.name}
+          />
+      );
+      lastCategory = product.category;
     });
+
+    return (
+        <table>
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked);
   }
 
   render() {
     return (
         <form>
-          <label>
-            Пойду:
+          <input
+              type="text"
+              placeholder="Search..."
+              value={this.props.filterText}
+              onChange={this.handleFilterTextChange}
+          />
+          <p>
             <input
-                name="isGoing"
                 type="checkbox"
-                checked={this.state.isGoing}
-                onChange={this.handleInputChange} />
-          </label>
-          <br />
-          <label>
-            Количество гостей:
-            <input
-                name="numberOfGuests"
-                type="number"
-                value={this.state.numberOfGuests}
-                onChange={this.handleInputChange} />
-          </label>
+                checked={this.props.inStockOnly}
+                onChange={this.handleInStockChange}
+            />
+            {' '}
+            Only show products in stock
+          </p>
         </form>
     );
   }
 }
 
-class Calculator extends React.Component {
+class FilterableProductTable extends React.Component {
   constructor(props) {
     super(props);
-    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-    this.state = {temperature: '', scale: 'c'};
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
   }
 
-  handleCelsiusChange(temperature) {
-    this.setState({scale: 'c', temperature});
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
   }
 
-  handleFahrenheitChange(temperature) {
-    this.setState({scale: 'f', temperature});
+  handleInStockChange(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    })
   }
 
   render() {
-    const scale = this.state.scale;
-    const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-
     return (
         <div>
-          <TemperatureInput
-              scale="c"
-              temperature={celsius}
-              onTemperatureChange={this.handleCelsiusChange} />
-
-          <TemperatureInput
-              scale="f"
-              temperature={fahrenheit}
-              onTemperatureChange={this.handleFahrenheitChange} />
-
-          <BoilingVerdict
-              celsius={parseFloat(celsius)} />
+          <SearchBar
+              filterText={this.state.filterText}
+              inStockOnly={this.state.inStockOnly}
+              onFilterTextChange={this.handleFilterTextChange}
+              onInStockChange={this.handleInStockChange}
+          />
+          <ProductTable
+              products={this.props.products}
+              filterText={this.state.filterText}
+              inStockOnly={this.state.inStockOnly}
+          />
         </div>
     );
   }
 }
 
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>Вода закипит.</p>;
-  }
-  return <p>Вода не закипит.</p>
-}
 
-function toCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
-}
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
-function toFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
-}
-
-function tryConvert(temperature, convert) {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) {
-    return '';
-  }
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
-}
 
 function App() {
   return (
@@ -251,17 +178,8 @@ function App() {
 
         <img src={logo} className="App-logo" alt="logo" />
 
-        <NameForm />
-        <br/>
-        <EssayForm />
-        <br/>
-        <FlavorForm />
-        <br/>
-        <Reservation />
-        <br/>
-        <Calculator />
-        <br/>
-        <input type="file" />
+        <FilterableProductTable products={PRODUCTS} />
+
       </header>
     </div>
   );
